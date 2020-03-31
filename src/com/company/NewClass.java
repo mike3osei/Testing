cc9a8fe97283d628f7bc2ac07221b627ee1edf49
syrenetidelacruz <syrenetidelacruz4@gmail.com>
2020-03-31 00:15:10 -0400

    Syreneti New message for Mike YUR

---
+++ src/com/company/NewClass.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class NewClass {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hi Eva");
+        System.out.println("Helloooo");
+        System.out.println("Hi Mike");
+        System.out.println("YURRR");
+    }
+
+}
+
+
